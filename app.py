8a4836aed63c139ee21bca8b4ca6bef298c815df
Kp270705 <kunalpathak4774@gmail.com>
2024-10-18 22:57:56 +0530

    change design

--- app.py
+++ app.py
@@ -5,54 +5,54 @@ app = Flask(__name__)
 
 @app.route('/')
 def landing():
-    return render_template('index.html')
+    return render_template('BaseFiles/landing.html')
 
 
-@app.route('/home')
-def home():
-    return render_template('home.html')
+@app.route('/notesType')
+def notesType():
+    return render_template('SelectNotes/notesCategory.html')
 
 @app.route('/selectPdf')
 def selectPdf():
-    return render_template('selectPdf.html')
+    return render_template('PdfNotes/selectPdf.html')
 
 @app.route('/selectText')
 def selectText():
-    return render_template('selectText.html')
+    return render_template('TextNotes/selectText.html')
 
 
 # ai routes: 
 @app.route('/aiText')
 def aiText():
-    return render_template('ai/ai_selection.html')
+    return render_template('TextNotes/ai/ai_selection.html')
 
 @app.route('/aiIntro')
 def ai_intro():
-    return render_template('ai/ai_introduction.html')
+    return render_template('TextNotes/ai/ai_introduction.html')
 
 @app.route('/aiAlgo')
 def ai_algo():
-    return render_template('ai/ai_algo.html')
+    return render_template('TextNotes/ai/ai_algo.html')
 
 @app.route('/aiNlp')
 def ai_nlp():
-    return render_template('ai/ai_nlp.html')
+    return render_template('TextNotes/ai/ai_nlp.html')
 
 @app.route('/aiRnn_Cnn')
 def ai_rnn_cnn():
-    return render_template('ai/ai_rnn_cnn.html')
+    return render_template('TextNotes/ai/ai_rnn_cnn.html')
 
 
 # dbms routes:
 
 @app.route('/dbmsText')
 def dbmsText():
-    return render_template('dbms/dbms_selection.html')
+    return render_template('TextNotes/dbms/dbms_selection.html')
 
 @app.route('/dbmsIntro')
 def dbms_intro():
-    return render_template('dbms/dbms_introduction.html')
+    return render_template('TextNotes/dbms/dbms_introduction.html')
 
 @app.route('/dbmsTypes')
 def dbms_types():
-    return render_template('dbms/dbms_types.html')
+    return render_template('TextNotes/dbms/dbms_types.html')
